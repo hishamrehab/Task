0badf5505a686b875e275950cd3beb17703ccdac
My Name <hishamrehab309@gmail.com>
2024-08-29 04:09:34 +0300

    Fix image compoent

--- src/app/components/Imagecomponent/ImageComponent.tsx
+++ src/app/components/Imagecomponent/ImageComponent.tsx
@@ -1,6 +1,7 @@
 import Img from "../../../../public/images/Rectangle 1 (2).png";
 
 import Image from "next/image";
+
 const ImageComponent = () => {
   return (
     <Image src={Img} alt="" className="absolute -z-50 h-screen	w-full top-0" />
